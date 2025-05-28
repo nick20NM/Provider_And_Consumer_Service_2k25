e269c99c8a0984466dfa1621c5aa45490e499967
Nick <masihnishit@gmail.com>
2025-05-28 23:03:01 +0530

    one endpoint created-> http://localhost:8081/hello

--- MicroserviceA/src/main/java/com/alpha/www/MicroserviceA/MicroserviceAApplication.java
+++ MicroserviceA/src/main/java/com/alpha/www/MicroserviceA/MicroserviceAApplication.java
@@ -2,12 +2,20 @@ package com.alpha.www.MicroserviceA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MicroserviceAApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceAApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello from Service A!";
 	}
 
 }
